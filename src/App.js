cc2f649779cc703427ed33da1a621b2c2f5b6e18
Shaghayegh Ghavami <ghavami.shgh@gmail.com>
2023-02-03 10:30:44 +0330

    Fetch countries data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
+import React, { useState, useEffect } from "react";
+import { useQuery } from "@apollo/client";
+import { LIST_COUNTRIES } from "./queries/countries-queries";
+import Container from "@mui/material/Container";
+
+
 function App() {
+  const [countries, setCountries] = useState([]);
+  const { data, loading, error } = useQuery(LIST_COUNTRIES);
+
+  useEffect(() => {
+    if (data?.countries) {
+      setCountries(data.countries);
+    }
+  }, [data]);
+
+  if (error) return 'error';
+
   return (
-    <h1>Welcome!</h1>
+      <Container sx={{ mt: 5, mb: 5, minHeight: "100vh" }}>
+        {loading ? 'loading': 
+        countries.map((con,i) =>  <div key={i}>{con.name}</div> )}
+      </Container>
+
   );
 }
 
