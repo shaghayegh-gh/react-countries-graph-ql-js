8a2a202c3c1e145f672a596006c7ca7e7a32c1b6
Shaghayegh Ghavami <ghavami.shgh@gmail.com>
2023-02-03 10:22:25 +0330

    Add apollo

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
-import { render } from 'react-dom';
-import CssBaseline from '@mui/material/CssBaseline';
-import App from './App';
+import { render } from "react-dom";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import CssBaseline from "@mui/material/CssBaseline";
+import App from "./App";
 
+const client = new ApolloClient({
+  uri: process.env.REACT_APP_API_URL,
+  cache: new InMemoryCache(),
+});
 
 render(
+  <ApolloProvider client={client}>
     <CssBaseline>
       <App />
-    </CssBaseline>,
-  document.getElementById('root')
+    </CssBaseline>
+  </ApolloProvider>,
+  document.getElementById("root")
 );
